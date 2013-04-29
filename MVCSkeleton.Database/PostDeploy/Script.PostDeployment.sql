7eb2bad282c54dea83b096fa76942a085ae41c64
Cristi <cristian.ochisor@gmail.com>
2013-04-29 16:51:46 +0300

    Revert removal

---
+++ MVCSkeleton.Database/PostDeploy/Script.PostDeployment.sql
@@ -0,0 +1,15 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+Insert INTO dbo.Users (Id,Name,Password) 
+Values (3,'User','Pwd@123');
+Insert INTO dbo.Users (Id,Name,Password) 
+Values (4,'TestUser','Pwd@123')
